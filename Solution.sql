c969956aadeefbc33d16f3e8ba5376520f586234
Alper Kaya <73350743+alperkaya0@users.noreply.github.com>
2022-01-22 00:33:37 +0300

    Update Solution.sql

--- Solution.sql
+++ Solution.sql
@@ -45,7 +45,3 @@ TURKISH:
 --actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
 --actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
 --İlk 3 sorguyu tekrar eden veriler için de yapalım.
-
-
-
-
